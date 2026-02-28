645eec8638618298def5775bb9dd2aa728a489eb
RajeshGajengi <gajengi95@gmail.com>
2026-02-28 12:00:51 +0530

    browserrouter update

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Muscle-Max-Fitness/>
+      <BrowserRouter basename="/Muscle-Max-Fitness/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
